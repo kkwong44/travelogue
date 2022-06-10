bfa46cc4477742f6eaf97d1ac28def994e0f3502
kkwong44 <wong.k@sky.com>
2022-06-10 19:54:01 +0000

    Create superuser and setup control panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,40 @@
+'''
+Administration to Django Control Panel
+'''
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Comment
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    '''
+    Manage Post Model and use django summernote for the content
+    '''
+    list_display = ('title', 'country', 'status', 'created_on')
+    search_fields = ['title', 'country', 'content']
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    '''
+    Manage Comment Model
+    '''
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    search_fields = ['name', 'email', 'body']
+    list_filter = ('approved', 'created_on')
+    actions = ['approve_comments', 'disapprove_comments']
+
+    def approve_comments(self, request, queryset):
+        '''
+        Bulk action to approve comments
+        '''
+        queryset.update(approved=True)
+
+    def disapprove_comments(self, request, queryset):
+        '''
+        Bulk action to disapprove comments
+        '''
+        queryset.update(approved=False)
